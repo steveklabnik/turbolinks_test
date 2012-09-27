34c02859935195685b2e5f7b17897dd760707aa1
Steve Klabnik <steve@steveklabnik.com>
2012-09-27 15:52:45 -0700

    initial repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TurbolinksTest::Application.config.secret_token = 'd351ae3a13c2408612434bc76fe3322543690b147575a5e191993c7c72f8472933046bdad18aa03d19caa9f57832a0575db5c71e592621af4aa3c14dd53fea77'
